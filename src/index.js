4f824db82ab0d9e57f845196b8870100791d5e35
Linsey R <email": null>
2017-04-27 20:54:04 -0700

    Create Mock Parallax background

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import GameState from 'states/GameState';
+import Boot from 'states/Boot';
+import Preload from 'states/Preload';
+import Main from 'states/Main';
 
 class Game extends Phaser.Game {
 
 	constructor() {
-		super(500, 500, Phaser.AUTO, 'content', null);
-		this.state.add('GameState', GameState, false);
-		this.state.start('GameState');
+
+		super(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO);
+
+		this.state.add('Boot', Boot, false);
+		this.state.add('Preload', Preload, false);
+		this.state.add('Main', Main, false);
+
+
+		this.state.start('Boot');
 	}
 
 }
