956a2255bb3c44ccb99f94b5510d635ec11c9585
Linsey R <email": null>
2017-04-28 15:09:26 -0700

    generate sky background

--- src/states/Main.js
+++ src/states/Main.js
@@ -1,13 +1,17 @@
 class Main extends Phaser.State {
+	constructor(){
+		super();
 
+	}
 	create() {
 
 		//Enable Arcade Physics
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
 		//Set the games background colour
-		this.game.stage.backgroundColor = '#4f90c1';
-		this.game.add.sprite('sky');
+		this.game.stage.backgroundColor = '#c8f2fb';
+		this.sky = this.game.add.sprite(0,0,'sky');
+		this.sky.scale.setTo(5,5);
 
 		this.mountainsBack = this.game.add.tileSprite(0,
 			this.game.height - this.game.cache.getImage('mountains-back').height,
@@ -36,6 +40,18 @@ class Main extends Phaser.State {
 			this.game.cache.getImage('ground-front').height,
 			'ground-front'
 		);
+
+
+
+		// this.game.physics.enable(this.tempRock);
+		this.addRocks();
+	}
+
+	addRocks() {
+		// Generate Obstacles
+		this.tempRock = this.game.add.sprite(this.game.world.randomX,
+			(this.game.height - this.groundFront.height) + 200, 'rock');
+		this.tempRock.scale.setTo(3, 3);
 	}
 
 	update() {
@@ -43,6 +59,8 @@ class Main extends Phaser.State {
 		this.hillsMid1.tilePosition.x -= 0.3;
 		this.fenceMid2.tilePosition.x -= 0.75;
 		this.groundFront.tilePosition.x -= 15.0;
+
+		this.addRocks();
 
 	}
 
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -5,12 +5,9 @@ class Preload extends Phaser.State {
 		this.game.load.image('hills-mid1', 'assets/hills-mid1.png');
 		this.game.load.image('fence-mid2', 'assets/fence-mid2.png');
 		this.game.load.image('ground-front', 'assets/ground-front.png');
-<<<<<<< HEAD
 		this.game.load.image('rock', 'assets/rock.gif');
-=======
-		this.game.load.image('sky', 'sky.png');
->>>>>>> d728164d82582be089f73a7d3d71e1053d4a352c
-		
+		this.game.load.image('sky', 'assets/sky.png');
+
 	}
 
 	create() {
