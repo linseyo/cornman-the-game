a7259bb2c132daa7debe80f61dae83f7cf6ba602
Julia(e) Xu <email": null>
2017-04-28 11:51:14 -0700

    Create tap to start functionality

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -2,7 +2,6 @@ class Boot extends Phaser.State {
 
 	create() {
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-
 		this.game.state.start("Preload");
 	}
 
--- src/states/Menu.js
+++ src/states/Menu.js
@@ -1,12 +1,10 @@
 class Menu extends Phaser.State {
 
-  create() {
-		// this.startButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY, 'start-button');
-    //
-		// this.startButton.input.onDown.add(startGame, this);
-
-			this.game.state.start("Main");
-	}
+  update() {
+    if(this.game.input.activePointer.justPressed()) {
+      this.game.state.start('Main');
+    }
+  }
 
 }
 
