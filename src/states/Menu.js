4b4a093d6a8b6799091a5c3c7601a4fc3feaa232
Julia(e) Xu <email": null>
2017-04-28 14:13:05 -0700

    Create scrolling upward corn

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -5,6 +5,8 @@ class Menu extends Phaser.State {
     this.menuBackground = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'menu-background');
     this.menuBackground.autoScroll(0, -20);
     this.menuBackground.alpha = 0.5;
+
+    this.startText = this.game.add.text(this.game.width/2, this.game.height/2, "Tap to begin", { font: "80px Arial", fill: "#fff", align: "center" });
   }
 
   update() {
