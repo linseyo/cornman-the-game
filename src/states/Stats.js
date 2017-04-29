662ce1968d139f1fbf40db93aeda8ba13ad2cb9c
jmxu94 <jmxu94@gmail.com>
2017-04-29 16:14:15 -0700

    Position stats page text

--- src/states/Stats.js
+++ src/states/Stats.js
@@ -4,8 +4,10 @@ class Stats extends Phaser.State {
 	create() {
     this.game.stage.backgroundColor = '#DFF4FF';
 
-    let text = "U ded, tap to continue"
-    this.startText = this.game.add.text(this.game.width/2, this.game.height/2, text, { font: "50px Arial", textalign: "center"});
+		let statsHeader = "STATS"
+    let continuePhrase = "Tap to Continue"
+    this.statsHeaderText = this.game.add.text(650, 50, statsHeader, { font: "250px Revalia", textalign: "center"});
+		this.continueText = this.game.add.text(820, 300, continuePhrase, { font: "50px Arial", textalign: "center"});
 	}
 
   update() {
