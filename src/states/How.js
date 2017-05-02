197d5392f36171a21c989ea124abfd671a5d483c
jmxu94 <jmxu94@gmail.com>
2017-05-02 16:04:38 -0700

    Fix how to play and back button

--- src/states/How.js
+++ src/states/How.js
@@ -1,10 +1,10 @@
 class How extends Phaser.State {
   create() {
     this.game.stage.backgroundColor = '#DFF4FF';
-    this.goBack = this.game.add.button(this.game.width-1700, this.game.height-300, 'go-back', this.goBack, this);
+    this.goBack = this.game.add.button(this.game.width-1700, this.game.height-300, 'go-back', this.back, this);
   }
 
-  goBack() {
+  back() {
     this.game.state.start('Menu');
   }
 }
