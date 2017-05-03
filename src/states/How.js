99fecedb95b01e07825d162aa8e68f3c035d86f7
Julia(e) Xu <email": null>
2017-05-03 10:44:23 -0700

    Resize how stage

--- src/states/How.js
+++ src/states/How.js
@@ -2,15 +2,14 @@ class How extends Phaser.State {
   create() {
     this.game.stage.backgroundColor = '#DFF4FF';
 
-    this.instruction = this.game.add.sprite(this.game.width/1.7, this.game.heightHalf, 'instruction');
+    this.instruction = this.game.add.sprite(this.game.widthHalf, this.game.heightHalf - 45, 'instruction');
     this.instruction.anchor.setTo(0.5);
 
     this.instruction.animations.add('show');
 		this.instruction.animations.play('show', 3, true);
 
-    this.goBack = this.game.add.button(this.game.width/8, this.game.heightHalf, 'go-back', this.back, this);
+    this.goBack = this.game.add.button(this.game.widthHalf, this.game.height - 60, 'go-back', this.back, this);
     this.goBack.anchor.setTo(0.5);
-    this.goBack.scale.setTo(this.game.aspectRatio/2, this.game.aspectRatio/2);
   }
 
   back() {
