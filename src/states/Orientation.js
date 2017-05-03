1ee80d2520bf6cdac916e880a4a187474b6e8eba
Joshua Mun <email": null>
2017-05-03 11:10:23 -0700

    turn orientation image

--- src/states/Orientation.js
+++ src/states/Orientation.js
@@ -1,8 +1,21 @@
 class Orientation extends Phaser.State {
   create() {
     this.game.stage.backgroundColor = '#DFF4FF';
-    this.game.add.image(100 , 100, 'orientation')
+    this.game.add.image(0 , 0, 'orientation');
+    this.game.scale.forceOrientation(true, false);
+    // if(game.device.desktop === false) {
+      if(this.game.scale.incorrectOrientation){
+
+      }
+      else {
+        this.startGame();
+      }
+    // }
+    // else {
+      // this.startGame();
+    // }
   }
+
 
   startGame() {
     this.game.state.start('Menu');
