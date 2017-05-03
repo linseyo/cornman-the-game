a999a8d76e136ffb8e78483e5c01f3daf7e128b0
Julia(e) Xu <email": null>
2017-05-03 10:16:13 -0700

    Resize instructions.png for game size

--- src/states/How.js
+++ src/states/How.js
@@ -2,16 +2,15 @@ class How extends Phaser.State {
   create() {
     this.game.stage.backgroundColor = '#DFF4FF';
 
-    this.instruction = this.game.add.sprite(this.game.width/1.6, this.game.heightHalf, 'instruction');
+    this.instruction = this.game.add.sprite(this.game.width/1.7, this.game.heightHalf, 'instruction');
     this.instruction.anchor.setTo(0.5);
-    this.instruction.scale.setTo(this.game.aspectRatio/2.5, this.game.aspectRatio/2.5);
 
     this.instruction.animations.add('show');
 		this.instruction.animations.play('show', 3, true);
 
-    this.goBack = this.game.add.button(this.game.width/6, this.game.heightHalf, 'go-back', this.back, this);
+    this.goBack = this.game.add.button(this.game.width/8, this.game.heightHalf, 'go-back', this.back, this);
     this.goBack.anchor.setTo(0.5);
-    this.goBack.scale.setTo(this.game.aspectRatio, this.game.aspectRatio);
+    this.goBack.scale.setTo(this.game.aspectRatio/2, this.game.aspectRatio/2);
   }
 
   back() {
