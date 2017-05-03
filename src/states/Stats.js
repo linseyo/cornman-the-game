c9d976911621f5f950cfa310e00337d7fb01889d
linseyo <reedlinsey@gmail.com>
2017-05-02 23:47:24 -0700

    Rescale Stats Page

--- src/states/Stats.js
+++ src/states/Stats.js
@@ -8,25 +8,35 @@ class Stats extends Phaser.State {
 
     this.game.stage.backgroundColor = '#DFF4FF';
 
-		this.enemyScore = this.game.add.text(screen.width / 3, screen.height / 3, ("Enemies dodged: " + this.enemyScore), { font: "12px Arial", fill: "#fffff"});
-		this.coinScore = this.game.add.text(screen.width / 3, screen.height / 3 + 10, ("Coins collected: " + this.coinScore), { font: "12px Arial", fill: "#fffff"});
-		this.playerScore = this.game.add.text(screen.width / 3, screen.height / 3 + 20, ("Total Score: " + this.totalScore), { font: "12px Arial", fill: "#fffff"});
-		// this.ammoScore = this.game.add.text(1000, 900, ("Bullets Fired: " + this.ammoScore), { font: "60px Arial", fill: "#fffff"});
-		this.sendButton = this.game.add.button(screen.width / 3, screen.height / 3 + 30, 'send', this.sendScore, this);
-		// this.sendButton.scale.setTo(screen.width / 4, screen.height / 4);
+		this.headerImage = this.game.add.image(this.game.widthHalf, screen.height/4, 'playerStat', 'assets/playerstats-title.png');
+		this.headerImage.anchor.setTo(0.5)
 
-		this.cornman = this.game.add.sprite( screen.width / 2 + 10 , screen.width / 2 + 10, 'cornman');
-		// this.cornman.scale.setTo( screen.width / 2, screen.height / 2 );
+
+		this.enemyScore = this.game.add.text(this.game.widthHalf, (this.headerImage.y) + (this.headerImage.height/ 9), ("Enemies dodged: " + this.enemyScore), { font: "12px Arial", fill: "#fffff"});
+		this.coinScore = this.game.add.text(this.game.widthHalf, (this.enemyScore.y) + 10, ("Coins collected: " + this.coinScore), { font: "12px Arial", fill: "#fffff"});
+		this.playerScore = this.game.add.text(this.game.widthHalf, (this.coinScore.y) + 10, ("Total Score: " + this.totalScore), { font: "12px Arial", fill: "#fffff"});
+		// this.ammoScore = this.game.add.text(1000, 900, ("Bullets Fired: " + this.ammoScore), { font: "60px Arial", fill: "#fffff"});
+		this.sendButton = this.game.add.button(this.game.widthHalf, (this.playerScore.y) + (this.playerScore.height) + 10, 'send', this.sendScore, this);
+		this.sendButton.anchor.setTo(0.5);
+		this.sendButton.scale.setTo(this.game.aspectRatio / 4, this.game.aspectRatio / 4)
+
+		this.enemyScore.anchor.setTo(0.5);
+		this.coinScore.anchor.setTo(0.5);
+		this.playerScore.anchor.setTo(0.5);
+
+		this.headerImage.scale.setTo(this.game.aspectRatio / 3, this.game.aspectRatio / 3)
+
+		this.lowerImage = this.game.add.image( this.game.widthHalf , (this.sendButton.y) + (this.sendButton.height) + 10, 'taptocont', 'assets/taptocont.png');
+		this.lowerImage.anchor.setTo(0.5)
+		this.lowerImage.scale.setTo(this.game.aspectRatio / 2, this.game.aspectRatio / 2)
+
+		this.cornman = this.game.add.sprite(this.game.width /3 , (this.lowerImage.y) + (this.lowerImage.height) + 10, 'cornman');
+		this.cornman.anchor.setTo(0.5)
+		this.cornman.scale.setTo(this.game.aspectRatio / 3, this.game.aspectRatio / 3)
 		this.cornman.animations.add('left', [0, 1, 2, 3, 4, 5, 6], 5, true);
 		this.cornman.animations.add('right', [0, 1, 2, 3, 4, 5, 6], 5, true);
 		this.cornman.animations.play('left')
 		this.game.add.tween(this.cornman).to( { x: this.cornman.x + 300 }, 2000, Phaser.Easing.Linear.None, true);
-
-		this.headerImage = this.game.add.image( screen.width / 4 , screen.width / 2, 'playerStat', 'assets/playerstats-title.png');
-
-		this.lowerImage = this.game.add.image( screen.width / 4 , screen.width / 2, 'taptocont', 'assets/taptocont.png');
-		// this.lowerImage.scale.setTo(screen.width / 3, screen.height / 3 );
-		// this.headerImage.scale.setTo(screen.width / 3, screen.height / 3 );
 	}
 
 	sendScore() {
