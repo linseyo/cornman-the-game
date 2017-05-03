ffc3f3c63246897de61856c7edfcb3d4d34d4816
jmxu94 <jmxu94@gmail.com>
2017-05-02 21:47:21 -0700

    Scale background corn scroll

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -7,12 +7,16 @@ class Menu extends Phaser.State {
   create() {
     this.game.stage.backgroundColor = '#DFF4FF';
 
-    this.menuBackground = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'menu-background');
-    this.menuBackground.scale.setTo(3.06, 3.06)
-    this.menuBackground.autoScroll(0, -20);
+    this.menuBackground = this.game.add.tileSprite(0, 0, screen.width, screen.width, 'menu-background');
+    this.menuBackground.scale.setTo(1.02, 1.02)
+    // console.log(this.game.width);
+    // console.log(this.game.height);
+    // console.log(screen.width);
+    // console.log(screen.height);
+    this.menuBackground.autoScroll(0, -15);
     this.menuBackground.alpha = 0.5;
 
-    let title = this.game.add.image(250, 200, 'cm-title');
+    let title = this.game.add.image(screen.width*0.5, screen.height*0.5, 'cm-title');
     title.scale.setTo(3, 3);
     this.startButton = this.game.add.button(this.game.width-1700, this.game.height-500, 'start-game', this.startGame, this);
     this.howButton = this.game.add.button(this.game.width-1700, this.game.height-300, 'how-to', this.startHow, this);
