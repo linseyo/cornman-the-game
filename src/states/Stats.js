def612f5335cc4c83e6a35984c0d44b8a5f6d17d
linseyo <reedlinsey@gmail.com>
2017-05-02 22:36:32 -0700

    start rescale

--- src/states/Stats.js
+++ src/states/Stats.js
@@ -8,20 +8,20 @@ class Stats extends Phaser.State {
 
     this.game.stage.backgroundColor = '#DFF4FF';
 
-		this.enemyScore = this.game.add.text(1000, 600, ("Enemies dodged: " + this.enemyScore), { font: "60px Arial", fill: "#fffff"});
-		this.coinScore = this.game.add.text(1000, 700, ("Coins collected: " + this.coinScore), { font: "60px Arial", fill: "#fffff"});
-		this.playerScore = this.game.add.text(1000, 800, ("Total Score: " + this.totalScore), { font: "60px Arial", fill: "#fffff"});
+		this.enemyScore = this.game.add.text(screen.width / 2, screen.height / 2, ("Enemies dodged: " + this.enemyScore), { font: "60px Arial", fill: "#fffff"});
+		this.coinScore = this.game.add.text(screen.width / 2, screen.height / 2 + 70, ("Coins collected: " + this.coinScore), { font: "60px Arial", fill: "#fffff"});
+		this.playerScore = this.game.add.text(screen.width / 2, screen.height / 2 + 80, ("Total Score: " + this.totalScore), { font: "60px Arial", fill: "#fffff"});
 		// this.ammoScore = this.game.add.text(1000, 900, ("Bullets Fired: " + this.ammoScore), { font: "60px Arial", fill: "#fffff"});
-		this.sendButton = this.game.add.button(1000, 1000, 'send', this.sendScore, this);
+		this.sendButton = this.game.add.button(screen.width / 2, screen.height / 2 + 100, 'send', this.sendScore, this);
 
-		this.cornman = this.game.add.sprite(800, 1300, 'cornman');
+		this.cornman = this.game.add.sprite(10, 130, 'cornman');
 		this.cornman.animations.add('left', [0, 1, 2, 3, 4, 5, 6], 5, true);
 		this.cornman.animations.add('right', [0, 1, 2, 3, 4, 5, 6], 5, true);
 		this.cornman.animations.play('left')
 		this.game.add.tween(this.cornman).to( { x: this.cornman.x + 300 }, 2000, Phaser.Easing.Linear.None, true);
 
-		this.headerImage = this.game.add.image(250, 100, 'playerStat', 'assets/playerstats-title.png');
-		this.lowerImage = this.game.add.image(925, 1200, 'taptocont', 'assets/taptocont.png');
+		this.headerImage = this.game.add.image(25, 10, 'playerStat', 'assets/playerstats-title.png');
+		this.lowerImage = this.game.add.image(92, 120, 'taptocont', 'assets/taptocont.png');
 	}
 
 	sendScore() {
