749a16615f29a5ac577ab0137c0345ca0eefb79b
Joshua Mun <email": null>
2017-05-03 11:23:24 -0700

    Add conditional logic to start if on desktop, once orientation correct will start game, all else will start game

--- src/states/Orientation.js
+++ src/states/Orientation.js
@@ -3,19 +3,18 @@ class Orientation extends Phaser.State {
     this.game.stage.backgroundColor = '#DFF4FF';
     this.game.add.image(0 , 0, 'orientation');
     this.game.scale.forceOrientation(true, false);
-    // if(game.device.desktop === false) {
+    if(this.game.device.desktop === false) {
       if(this.game.scale.incorrectOrientation){
-
+        this.game.scale.onOrientationChange.add(this.startGame, this);
       }
       else {
         this.startGame();
       }
-    // }
-    // else {
-      // this.startGame();
-    // }
+    }
+    else {
+      this.startGame();
+    }
   }
-
 
   startGame() {
     this.game.state.start('Menu');
