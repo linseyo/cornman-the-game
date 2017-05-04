0bdfe673ead3a2f1b730cfdfa9b25d21a18fe60b
Joshua Mun <email": null>
2017-05-03 21:26:33 -0700

    add conditional logic to change orientation

--- src/states/Orientation.js
+++ src/states/Orientation.js
@@ -1,15 +1,26 @@
 class Orientation extends Phaser.State {
+  // constructor() {
+  //   super()
+  //   this.timer = this.game.time.now;
+  // }
   create() {
     this.moozic = this.game.add.audio('banjo');
     this.moozic.play();
 
     this.game.stage.backgroundColor = '#DFF4FF';
-    this.game.add.image(0 , 0, 'orientation');
+    let turnImage = this.game.add.image(0 , 0, 'orientation');
+    turnImage.x = 0;
+    turnImage.y = 0;
+    turnImage.height = 640;
+    turnImage.width = 960;
     this.game.scale.forceOrientation(true, false);
-      if(this.game.scale.incorrectOrientation){
-        this.game.scale.onOrientationChange.add(this.startGame, this);
+      if(window.innerWidth > window.innerHeight){
+        this.startGame();
       }
-      else {
+    }
+
+    update() {
+      if(this.game.innerWidth < this.game.innerHeight){
         this.startGame();
       }
     }
